1afdf1df74d8b1d5044825a411851ed06a4e52b2
Duranskinho <duranski17@gmail.com>
2017-12-07 14:34:56 +1300

    added previously written code
    core functionality works, many areas of improvement are possible

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from dice import Dice
+
+def main():
+    dice = Dice()
+    dice.loop()
+
+    print("thanks for playing!")
+    exit(0)
 
 if __name__ == '__main__':
-    print("hello mother fuckers!")
+    main()
